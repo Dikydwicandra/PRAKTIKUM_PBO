76e8a7936e22b23a46a30392a782c17731b44edd
Dikydwicandra <113652422+Dikydwicandra@users.noreply.github.com>
2023-03-07 15:52:17 +0800

    ADD

---
+++ POSTTEST1/Data.java
@@ -0,0 +1,24 @@
+package POSTTEST1;
+
+public class Data {
+    String nama;
+    String warna;
+    int kapasitas;
+    int harga;
+    
+    public Data(String nama, String warna, int kapasitas, int harga){
+        this.nama = nama;
+        this.warna = warna;
+        this.kapasitas = kapasitas;
+        this.harga = harga;
+    }
+    
+    public void simpandata(){
+        System.out.println("Data Berhasil Disimpan");
+        
+    }
+
+    
+          
+    
+}
---
+++ POSTTEST1/Hapus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POSTTEST1;
+
+/**
+ *
+ * @author ROG
+ */
+public class Hapus {
+    void tanyaidx(){
+        System.out.print(" Hapus Data Hp ke : ");
+    
+}
+    
+}
